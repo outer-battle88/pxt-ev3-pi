e57619d3d7ebf9fe6598357b99b4e5dfd19deaa2
outer-battle88 <bensonyoast@gmail.com>
2025-12-16 19:13:19 -0800

    Fix EV3 screen display for STT

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 //% color=160 weight=100 icon="\uf130"
 namespace STT {
-    let lastCommand = ""
+    let lastCommand: string = ""
 
     /**
      * Set a command from Raspberry Pi
@@ -12,7 +12,7 @@ namespace STT {
     }
 
     /**
-     * Get last received command
+     * Get the last received command
      */
     //% block="get last command"
     export function getCommand(): string {
@@ -20,12 +20,12 @@ namespace STT {
     }
 
     /**
-     * Show last command on EV3 screen
+     * Show the last command on the EV3 screen
      */
     //% block="show last command"
-    export function showCommand() {
+    export function showLastCommand() {
         if (lastCommand != "") {
-            basic.showString(lastCommand)
+            brick.showString(lastCommand, 0) // line 0 of the screen
             lastCommand = ""
         }
     }
